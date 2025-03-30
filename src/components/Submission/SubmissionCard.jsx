31638925c123a757fcaab9d37e1b2ea6ac3229e2
Shubham Shudhanshu <shubham34934@gmail.com>
2025-03-30 23:58:30 +0530

    changes new home page

--- src/components/Submission/SubmissionCard.jsx
+++ src/components/Submission/SubmissionCard.jsx
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import styles from './SubmissionCard.module.css';
-import likeColored from "./../../assets/icons/like_colored.svg"
-const SubmissionCard = ({ image, title, views }) => {
+import likeColored from "./../../assets/icons/like_colored.svg";
+
+const SubmissionCard = ({ id, image, title, views }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/videos/${id}`);
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleCardClick}>
       <div className={styles.imageContainer}>
         <img src={image} alt={title} className={styles.image} />
       </div>
@@ -19,6 +27,7 @@ const SubmissionCard = ({ image, title, views }) => {
 };
 
 SubmissionCard.propTypes = {
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   views: PropTypes.string.isRequired

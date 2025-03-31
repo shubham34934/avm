f6e1fe7808afb5ffe8c4955dc85a6afd76bd2233
Shubham Shudhanshu <shubham34934@gmail.com>
2025-03-31 14:19:54 +0530

    Improve Vercel build configuration with custom build script

--- src/components/AppInitializer/AppInitializer.jsx
+++ src/components/AppInitializer/AppInitializer.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getAccount } from "../../reducers/authentication";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const AppInitializer = ({ children }) => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const [initialized, setInitialized] = useState(false);
 

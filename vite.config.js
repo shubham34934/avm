5b4eca72259ec89dec8fb741135fbf553c11a8de
Shubham Shudhanshu <shubham34934@gmail.com>
2025-03-31 13:30:56 +0530

    Update Vercel deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -11,6 +12,11 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env,
       'process.browser': true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
     server: {
       proxy: {

e665eab7a8e752e2a6327fbaa084f896fb192b8a
Shubham Shudhanshu <shubham34934@gmail.com>
2025-03-31 15:18:23 +0530

    loop issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,32 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode`
-  const env = loadEnv(mode, process.cwd(), '');
-  
+  const env = loadEnv(mode, process.cwd(), "");
+
   return {
+    base: "./", // Ensure Vite uses relative paths for assets
     plugins: [react()],
     define: {
-      'process.env': env,
-      'process.browser': true,
+      "import.meta.env": JSON.stringify(env),
+      "process.browser": true,
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:9001',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, '/api')
-        }
-      }
-    },
     build: {
-      outDir: 'dist',
-      sourcemap: mode !== 'production',
+      outDir: "dist",
+      sourcemap: mode !== "production",
       rollupOptions: {
         output: {
           manualChunks: {
-            vendor: ['react', 'react-dom', 'react-router-dom'],
-          }
-        }
-      }
-    }
+            vendor: ["react", "react-dom", "react-router-dom"],
+          },
+        },
+      },
+    },
   };
 });
